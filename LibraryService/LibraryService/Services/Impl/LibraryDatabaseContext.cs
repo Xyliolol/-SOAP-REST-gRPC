1bf12758943cb8d40ffdb4e840ab48718b55fef0
Xyliolol <93090627+Xyliolol@users.noreply.github.com>
2022-12-02 18:22:46 +0300

    Урок 1. SOAP, ASMX, SopaUI

---
+++ LibraryService/LibraryService/Services/Impl/LibraryDatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using LibraryService.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace LibraryService.Services.Impl
+{
+    public class LibraryDatabaseContext : ILibraryDatabaseContextService
+    {
+        private IList<Book> _LibraryDb;
+        public IList<Book> Books => _LibraryDb;
+
+        public LibraryDatabaseContext()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            _LibraryDb = (List<Book>)JsonConvert.DeserializeObject(Encoding.UTF8.GetString(Properties.Resources.books), typeof(List<Book>));
+        }
+    }
+}
---
+++ LibraryService/LibreryService.Web/Controllers/LibraryController.cs
@@ -0,0 +1,64 @@
+﻿using LibraryServiceReference;
+using LibreryService.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LibreryService.Web.Controllers
+{
+    public class LibraryController : Controller
+    {
+        private readonly ILogger<LibraryController> _logger;
+
+        public LibraryController(ILogger<LibraryController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(SearchType searchType, string searchString)
+        {
+            try
+            {
+                LibraryWebServiceSoapClient client = new LibraryWebServiceSoapClient(LibraryWebServiceSoapClient.EndpointConfiguration.LibraryWebServiceSoap);
+
+                if (!string.IsNullOrEmpty(searchString) && searchString.Length >= 3)
+                {
+                    switch (searchType)
+                    {
+                        case SearchType.Title:
+                            return View(new BookCategoryViewModel
+                            {
+                                Books = client.GetBooksByTitle(searchString)
+                            });
+                        case SearchType.Author:
+                            return View(new BookCategoryViewModel
+                            {
+                                Books = client.GetBooksByAuthor(searchString)
+                            });
+                        case SearchType.Category:
+                            return View(new BookCategoryViewModel
+                            {
+                                Books = client.GetBooksByCategory(searchString)
+                            });
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+
+            }
+            return View(new BookCategoryViewModel
+            {
+                Books = new Book[] { }
+            });
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
