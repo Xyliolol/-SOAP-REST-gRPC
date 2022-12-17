68e99c2022cde7a330e5c130016fa1e77736321c
Xyliolol <93090627+Xyliolol@users.noreply.github.com>
2022-12-17 12:09:00 +0300

    Расширяем свой сервис SingalR

---
+++ ClinicService/AccountSample/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using ClinicService.Utils;
+
+namespace AccountSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = PasswordUtils.CreatePasswordHash("1234");
+            Console.WriteLine(result.passwordSalt);
+            Console.WriteLine(result.passwordHash);
+            Console.ReadKey();
+        }
+    }
+}
